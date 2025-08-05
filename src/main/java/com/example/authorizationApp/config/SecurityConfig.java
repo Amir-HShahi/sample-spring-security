329bf6e7c3e68e6b6c65c3f69c4cd7b7d6df2bef
Amir-HShahi <amir13831107h@gmail.com>
2025-08-05 19:01:24 +0330

    stopped tracking of application properties

--- src/main/java/com/example/authorizationApp/config/SecurityConfig.java
+++ src/main/java/com/example/authorizationApp/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                         .requestMatchers("register", "login")
                         .permitAll()
                         .anyRequest().authenticated())
+                .oauth2Login(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
